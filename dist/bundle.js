19cdb114dae1f93511b1ece6889b1de86d6ed612
Yauhen Tretsyak <zhendos@tut.by>
2018-11-01 15:35:09 +0100

    Day one 15:35

--- dist/bundle.js
+++ dist/bundle.js
@@ -196,7 +196,29 @@ function img_click() {}
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return modal; });
-function modal() {}
+function modal() {
+  var body = document.querySelector('body'),
+      mdl = document.querySelector('.popup_engineer'),
+      btn_mdl = document.querySelector('.header_btn'),
+      btn_close = mdl.querySelector('strong');
+  /* body.addEventListener('click', function(e){
+       let target = e.target;
+             if(target.classList.contains('header_btn')){
+           mdl.style.display = 'block';
+       }
+             if(target.classList.contains('popup_close')) {
+           mdl.style.display = 'none';
+       }
+   });*/
+
+  btn_mdl.addEventListener('click', function () {
+    mdl.style.display = 'block';
+    console.log(btn_close);
+  });
+  btn_close.addEventListener('click', function () {
+    mdl.style.display = 'none';
+  });
+}
 
 /***/ }),
 
--- src/parts/modal.js
+++ src/parts/modal.js
@@ -1,3 +1,30 @@
 export default function modal() {
+    let body = document.querySelector('body'),
+        mdl = document.querySelector('.popup_engineer'),
+        btn_mdl = document.querySelector('.header_btn'),
+        btn_close = mdl.querySelector('strong');
+
     
+
+       /* body.addEventListener('click', function(e){
+            let target = e.target;
+    
+            if(target.classList.contains('header_btn')){
+                mdl.style.display = 'block';
+            }
+    
+            if(target.classList.contains('popup_close')) {
+                mdl.style.display = 'none';
+            }
+        });*/
+    
+    btn_mdl.addEventListener('click', function(){
+        mdl.style.display = 'block';
+        console.log(btn_close);
+    });
+
+    btn_close.addEventListener('click', function(){
+        mdl.style.display = 'none';
+    });
+
 }
