2074e6988db291ae9cf8d2568989e4a300dc3d32
Yauhen Tretsyak <zhendos@tut.by>
2018-11-01 10:54:50 +0100

    Day one 10:55

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import modal from './parts/modal';
+import forms_modal from './parts/forms_modal';
+import calc from './parts/calc';
+import forms_page from './parts/forms_page';
+import tabs_wind from './parts/tabs_wind';
+import tabs_finish from './parts/tabs_finish';
+import timer from './parts/timer';
+import img_click from './parts/img_click';
+import modal_time from './parts/modal_time';
+import modal_popup from './parts/modal_popup';
+
+window.addEventListener(DOMContentLoaded, function(){
+    modal();
+    forms_modal();
+    calc();
+    forms_page();
+    tabs_wind();
+    tabs_finish();
+    timer();
+    img_click();
+    modal_time();
+    modal_popup();
+});
---
+++ src/parts/forms_modal.js
@@ -0,0 +1,3 @@
+export default function forms_modal(){
+    
+}
