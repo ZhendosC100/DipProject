dc71e94820a951c6b5fabbf2a167bb60dde56ed3
Yauhen Tretsyak <zhendos@tut.by>
2018-11-03 17:52:13 +0100

    Day three calculator 17:55

--- dist/bundle.js
+++ dist/bundle.js
@@ -139,11 +139,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return calc; });
 function calc() {
   var calc = document.querySelector('.popup_calc'),
-      calc_profile = document.querySelector('.popup_calc_button'),
-      close_calc = calc.querySelector('.popup_calc_close'),
+      calc_profile = document.querySelector('.popup_calc_profile'),
+      calc_end = document.querySelector('.popup_calc_end'),
       inp_width = document.getElementById('width'),
       inp_height = document.getElementById('height'),
-      body_calc = document.querySelector('body');
+      calc_img_one = document.getElementById('type1'),
+      calc_img_two = document.getElementById('type2'),
+      calc_img_three = document.getElementById('type3'),
+      calc_img_four = document.getElementById('type4'),
+      //farm/cold cacl_profile
+  check_cold = document.getElementById('cold'),
+      check_warm = document.getElementById('warm'),
+      //end farm/cold cacl_profile
+  body_calc = document.querySelector('body');
+  calc_img_one.style.display = "block";
+  console.log(calc_end);
+  console.log(calc);
 
   function replace() {
     this.value = this.value.replace(/[^\d]/ig, '');
@@ -152,19 +163,79 @@ function calc() {
   inp_width.oninput = replace;
   inp_height.oninput = replace;
   body_calc.addEventListener('click', function (event) {
+    //event.preventDefault();
     var target = event.target;
+    console.log(event.target);
 
     if (target.classList.contains('glazing_price_btn')) {
-      calc.style.display = 'block';
+      calc.style.display = 'block'; //calc open
     }
 
     if (target.classList.contains('popup_calc_button')) {
-      calc.style.display = 'none';
-      calc_profile.style.display = 'block';
+      event.preventDefault();
+      calc.style.display = 'none'; //calc close
+
+      calc_profile.style.display = 'block'; //calc_profile open
+      //check_cold.style.background = 'url(../img/modal_calc/check.png)';
+    }
+
+    if (target.classList.contains('popup_calc_profile_button')) {
+      event.preventDefault();
+      calc_profile.style.display = 'none'; //calc_profile close
+
+      calc_end.style.display = 'block'; //cacl_end open
+    }
+
+    if (target.classList.contains('popup_calc')) {
+      calc.style.display = 'none'; //calc close
+    }
+
+    if (target.classList.contains('popup_calc_profile')) {
+      calc_profile.style.display = 'none'; //calc_profile close
+    }
+
+    if (event.target.tagName == 'STRONG') {
+      calc.style.display = 'none'; //calc close
+
+      calc_profile.style.display = 'none'; //calc_profile close
+
+      calc_end.style.display = 'none'; //calc_end close
+    } //prevju
+
+
+    if (target.classList.contains('type1_img')) {
+      calc_img_one.style.display = "block";
+      calc_img_two.style.display = "none";
+      calc_img_three.style.display = "none";
+      calc_img_four.style.display = "none";
+    }
+
+    if (target.classList.contains('type2_img')) {
+      calc_img_one.style.display = "none";
+      calc_img_two.style.display = "block";
+      calc_img_three.style.display = "none";
+      calc_img_four.style.display = "none";
+    }
+
+    if (target.classList.contains('type3_img')) {
+      calc_img_one.style.display = "none";
+      calc_img_two.style.display = "none";
+      calc_img_three.style.display = "block";
+      calc_img_four.style.display = "none";
+    }
+
+    if (target.classList.contains('type4_img')) {
+      calc_img_one.style.display = "none";
+      calc_img_two.style.display = "none";
+      calc_img_three.style.display = "none";
+      calc_img_four.style.display = "block";
     }
   });
-  close_calc.addEventListener('click', function () {
-    calc.style.display = 'none';
+  check_cold.addEventListener('click', function () {
+    check_warm.classList.toggle('checkbox-custom');
+  });
+  check_warm.addEventListener('click', function () {
+    check_cold.classList.toggle('checkbox-custom');
   });
 }
 
--- src/parts/calc.js
+++ src/parts/calc.js
@@ -1,11 +1,23 @@
 export default function calc(){
 
     let calc = document.querySelector('.popup_calc'),
-        calc_profile = document.querySelector('.popup_calc_button'),
-        close_calc = calc.querySelector('.popup_calc_close'),
+        calc_profile = document.querySelector('.popup_calc_profile'),
+        calc_end = document.querySelector('.popup_calc_end'),
         inp_width = document.getElementById('width'),
         inp_height = document.getElementById('height'),
+        calc_img_one = document.getElementById('type1'),
+        calc_img_two = document.getElementById('type2'),
+        calc_img_three = document.getElementById('type3'),
+        calc_img_four = document.getElementById('type4'),
+        //farm/cold cacl_profile
+        check_cold = document.getElementById('cold'),
+        check_warm = document.getElementById('warm'),
+        //end farm/cold cacl_profile
         body_calc = document.querySelector('body');
+        calc_img_one.style.display = "block";
+
+        console.log(calc_end);
+        console.log(calc);
 
         function replace() {
             this.value = this.value.replace(/[^\d]/ig, '');
@@ -15,21 +27,82 @@ export default function calc(){
             inp_height.oninput = replace;
 
     body_calc.addEventListener('click', function(event){
+        //event.preventDefault();
         let target = event.target;
 
+        console.log(event.target);
+
         if(target.classList.contains('glazing_price_btn')){
-            calc.style.display = 'block';
+            calc.style.display = 'block';          //calc open
         }
 
         if(target.classList.contains('popup_calc_button')){
-            calc.style.display = 'none';
-            calc_profile.style.display = 'block';
+            event.preventDefault();
+            calc.style.display = 'none';          //calc close
+            calc_profile.style.display = 'block'; //calc_profile open
+            //check_cold.style.background = 'url(../img/modal_calc/check.png)';
+        }
+
+        if(target.classList.contains('popup_calc_profile_button')){
+            event.preventDefault();
+            calc_profile.style.display = 'none'; //calc_profile close
+            calc_end.style.display = 'block'; //cacl_end open
+
+        }
+
+        if(target.classList.contains('popup_calc')){
+            calc.style.display = 'none';         //calc close
+        } 
+
+        if(target.classList.contains('popup_calc_profile')){
+            calc_profile.style.display = 'none'; //calc_profile close
+        } 
+
+        if(event.target.tagName == 'STRONG') {
+            calc.style.display = 'none';         //calc close
+            calc_profile.style.display = 'none'; //calc_profile close
+            calc_end.style.display = 'none'; //calc_end close
+        }
+
+            //prevju
+        if(target.classList.contains('type1_img')){
+            calc_img_one.style.display = "block";
+            calc_img_two.style.display = "none";
+            calc_img_three.style.display = "none";
+            calc_img_four.style.display = "none";
+        }
+
+        if(target.classList.contains('type2_img')){
+            calc_img_one.style.display = "none";
+            calc_img_two.style.display = "block";
+            calc_img_three.style.display = "none";
+            calc_img_four.style.display = "none";
+        }
+
+        if(target.classList.contains('type3_img')){
+            calc_img_one.style.display = "none";
+            calc_img_two.style.display = "none";
+            calc_img_three.style.display = "block";
+            calc_img_four.style.display = "none";
+        }
+
+        if(target.classList.contains('type4_img')){
+            calc_img_one.style.display = "none";
+            calc_img_two.style.display = "none";
+            calc_img_three.style.display = "none";
+            calc_img_four.style.display = "block";
         }
     });
 
-    close_calc.addEventListener('click', function(){
-        calc.style.display = 'none';
+
+
+
+    check_cold.addEventListener('click', () => {
+            check_warm.classList.toggle('checkbox-custom');
     });
 
+    check_warm.addEventListener('click', () => {
+            check_cold.classList.toggle('checkbox-custom');
+    });
     
 }
